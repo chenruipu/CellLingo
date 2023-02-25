44f439558b26137a58203b5e73afc0f4a0768045
chenruipu <69501880+chenruipu@users.noreply.github.com>
2023-02-25 17:22:34 +0800

    Add files via upload

---
+++ CellLingo-main/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 16:48:59 2023
+
+@author: chenruipu
+"""
+
+import setuptools
+
+setuptools.setup(
+    name="cellLingo",
+    version="1.0",
+    author="Chen Ruipu",
+    author_email="chencuge@live.com",
+    description="",
+    long_description="CellLingo",
+    long_description_content_type="CellLingo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
